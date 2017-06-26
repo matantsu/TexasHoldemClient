3bd998466bc5045da32680edbd42875e797e1405
matantsu <matantsuberi@gmail.com>
2017-06-26 22:30:03 +0300

    asd

--- TexasHoldemClient/BusinessLayer/TestWindow.xaml.cs
+++ TexasHoldemClient/BusinessLayer/TestWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CefSharp;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,25 +25,6 @@ namespace TexasHoldemClient.BusinessLayer
         public TestWindow()
         {
             InitializeComponent();
-            BL.GameManager.PropertyChanged += GameManager_PropertyChanged;
-
-            g = BL.GameManager.Listen(37);
-            g.PropertyChanged += G_PropertyChanged;
-        }
-
-        private void G_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            
-        }
-
-        private void Browser_ConsoleMessage(object sender, ConsoleMessageEventArgs e)
-        {
-            MessageBox.Show(e.Line + e.Message + e.Source);
-        }
-
-        private void Browser_LoadError(object sender, CefSharp.LoadErrorEventArgs e)
-        {
-            MessageBox.Show(e.ErrorText + e.FailedUrl);
         }
     }
 }
